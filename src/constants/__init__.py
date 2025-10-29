4e2faf86c7bde3c712353c8af13e89cb56a2df6a
brij26 <brijraptel007@gmail.com>
2025-10-29 17:44:47 +0530

    Model trainer component added

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -55,6 +55,8 @@ MODEL TRAINER related constant start with MODEL_TRAINER var name
 MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_METRIC_ARTIFACT_DIR: str = "performance_metric"
+MODEL_TRAINER_METRIC_ARTIFACT_NAME: str = "performance.json"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join(
     "config", "model.yaml")
